9f51314de46ca58011eeb050bfb7d18b7c01d358
Maksim <maks.antonov.2012@gmail.com>
2022-06-12 20:01:01 +0300

    added methodSource and EnumSource

---
+++ src/test/java/tests/Films.java
@@ -0,0 +1,11 @@
+package tests;
+
+public enum Films {
+    GREEN_MILE("Зеленая миля"), SCHINDLER_LIST("Список Шиндлера"), SHAWSHANK_REDEMPTION("Побег из Шоушенка");
+
+    public final String film;
+
+    Films(String film) {
+        this.film = film;
+    }
+}
--- src/test/java/tests/KinopoiskTests.java
+++ src/test/java/tests/KinopoiskTests.java
@@ -1,12 +1,16 @@
 package tests;
 
+import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class KinopoiskTests extends TestBase{
 
@@ -18,14 +22,37 @@ public class KinopoiskTests extends TestBase{
         $("[data-index='0']").shouldHave(text(testData));
     }
 
+    @EnumSource(Films.class)
+    @ParameterizedTest(name = "При вводе в поле поиска фильма {0} в результатах на первом месте отображается {0}")
+    void checkAnyFilmsInTopWithEnum(Films film) {
+        Selenide.open("https://www.kinopoisk.ru/");
+        $("[name=kp_query]").setValue(film.film);
+        $("[data-index='0']").shouldHave(text(film.film));
+    }
+
     @CsvSource(value = {
             "gfwfys, Пацаны",
             "jabc, Офис"
     })
-    @ParameterizedTest(name = "пользователь случайно ввел название фильма {0} англиской раскладкой {1}")
+    @ParameterizedTest(name = "пользователь случайно ввел название фильма {1} англиской раскладкой {0}")
     void checkResultWithIncorrectInput(String incorrectName, String expectedResult) {
         Selenide.open("https://www.kinopoisk.ru/");
         $("[name=kp_query]").setValue(incorrectName);
         $("[data-index='0']").shouldHave(text(expectedResult));
+    }
+
+    @MethodSource()
+    @ParameterizedTest(name = "Проверяем, что фильмы {0} входят в первые 50 лучших фильмов")
+    void checkAnyFilmsInTop(List<String> films) {
+        Selenide.open("https://www.kinopoisk.ru/lists/movies/top250");
+        $$("[class='styles_mainTitle__IFQyZ styles_activeMovieTittle__kJdJj']")
+                .shouldHave(CollectionCondition.containExactTextsCaseSensitive(films));
+    }
+
+    private static Stream<Arguments> checkAnyFilmsInTop() {
+
+        return Stream.of(
+                Arguments.of(List.of("Зеленая миля", "Список Шиндлера", "Побег из Шоушенка"))
+        );
     }
 }
